970e3c7461694e81965e691eb3368159860cb8ce
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-04 14:39:57 -0300

    montando o desafio 12, dos números de telefone, falta verificar a repetiçoã

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,9 +16,25 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numbersPhone) {
   // seu código aqui
+  if (numbersPhone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (let index of numbersPhone) {
+      if (index < 0 || index > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  } 
+   
+  
+  console.log( `(${numbersPhone[0]}${numbersPhone[1]}) ${numbersPhone[2]}${numbersPhone[3]}${numbersPhone[4]}${numbersPhone[5]}${numbersPhone[6]}-${numbersPhone[7]}${numbersPhone[8]}${numbersPhone[9]}${numbersPhone[10]}`);
 }
+numbersPhone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+generatePhoneNumber(numbersPhone);
+
+
 
 // Desafio 12
 function triangleCheck() {
